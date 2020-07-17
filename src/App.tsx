182915946b03d4fd2cc0772317d6d7c36b9c8f42
chenxu950321 <56179261+chenxu950321@users.noreply.github.com>
2020-07-16 23:21:33 -0700

    scss develoment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <code>const a =b</code>
+    </div>
+  );
+}
+
+export default App;
